aead54c6afee02a3127c813b2969c97a3d8f3b56
Asuki Kono <asukiaaa@gmail.com>
2023-01-08 12:44:55 +0900

    Remove not used lines

--- extra.py
+++ extra.py
@@ -4,7 +4,6 @@ import subprocess
 
 try:
     cmd = "git log -1 --pretty=%cd --date=format:%Y-%m-%d-%H:%M:%S%z"
-    # str_info_git = subprocess.check_output(['git', 'log', '-1', "--pretty='format:\%cd'", "--date=format:'\%Y-\%m-\%d \%H:\%M:\%S'"]).strip()
     str_info_git = subprocess.check_output(cmd.split()).strip().decode('utf-8')
     cmd = 'git diff --name-only'
     str_diff_files = subprocess.check_output(cmd.split()).strip()
@@ -13,7 +12,6 @@ try:
     else:
         str_info_git = str_info_git + '-modified'
     print(str_info_git)
-    # print(str_info_git.decode('utf-8'))
     projenv.Append(CPPDEFINES=[("STR_INFO_GIT", env.StringifyMacro(str_info_git))])
 except Exception as e:
     print("cannot assign str")
